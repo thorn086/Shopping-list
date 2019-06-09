cb8ff2ac1f84bf4a48db58bac5d8207b7c1fd6de
Darren Rose <seeuagn@darrens-mbp-2.lan>
2019-06-08 22:05:22 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// adding items to list function
+
+function formListen(){
+    $('#js-shopping-list-form').submit(function(event){     
+    event.preventDefault();
+    const userText = $(event.currentTarget).find("#shopping-list-entry");
+    
+    const newBox = `  <li>
+    <span class="shopping-item">${userText.val()}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`
+    $(".shopping-list").append(newBox);
+    userText.val("");
+});
+}
+//check fucntion
+
+function checkingItem(){
+ $('.shopping-item-toggle').on('click', function(event){
+   event.stopPropagation();
+  $(this).toggleClass('shopping-item__checked');
+ });
+}
+//delete function
+
+function removeItem(){
+  $('ul').on('click','.shopping-item-delete', function(event){
+  $(this).closest('li').remove();
+});
+}
+
+//Callback functions
+
+$(formListen());
+$(removeItem());
+$(checkingItem());
+
